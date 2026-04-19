061e535fdb99953b518f8be426e5fee33033fcea
Subhash <Git: Enable>
2026-04-19 23:00:20 +0530

    Initial commit - YouTube Analyzer

--- server.py
+++ server.py
@@ -1627,5 +1627,5 @@ HTML_TEMPLATE = r"""
 
 if __name__ == "__main__":
     # uvicorn.run(app, host="0.0.0.0", port=8000)
-    app.run(host="0.0.0.0", port=8000)
-    
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
